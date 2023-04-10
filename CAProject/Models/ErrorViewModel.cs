889e4d9b026d82feb8a802bd6658b1b053be3a49
DESKTOP-DMRK48E\Bruce Kyaw <brucekyaw98@gmail.com>
2023-04-10 13:35:56 +0800

    Add project files.

---
+++ CAProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CAProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
